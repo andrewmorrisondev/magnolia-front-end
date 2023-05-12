06f2d97f5f5b983385a54b648413697c57747bc6
nonchalamment <nonchalamment@gmail.com>
2023-05-12 10:45:22 -0400

    remove comments from landing page

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -4,7 +4,6 @@ import styles from './Landing.module.css'
 const Landing = ({ user }) => {
   return (
     <main className={styles.container}>
-      {/* <h1>Welcome to Magnolia, {user ? user.name : 'guest!'}</h1> */}
       <h1>Magnolia</h1>
       <h2>{user ? 'Welcome, ' + user.name + '!' : 'Welcome, guest!'}</h2>
     </main>
