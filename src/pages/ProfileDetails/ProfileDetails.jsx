84eba7c0b3c72803212242e6332167886946076e
Andrew Morrison <morrison.andrew422@gmail.com>
2023-05-14 15:12:37 -0400

    fix bugs

--- src/pages/ProfileDetails/ProfileDetails.jsx
+++ src/pages/ProfileDetails/ProfileDetails.jsx
@@ -1,5 +1,6 @@
 // npm modules
 import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
 
 // components
 import NewTree from "../../components/NewTree/NewTree"
@@ -9,27 +10,28 @@ import * as treeService from '../../services/treeService'
 import * as profileService from '../../services/profileService'
 
 const ProfileDetails = (props) => {
-  const [profile, setProfile] = useState(props.user.profile)
+  const { profileId } = useParams()
+  const [profile, setProfile] = useState(null)
 
   const handleAddTree = async (treeFormData) => {
     const newTree = await treeService.create(treeFormData)
-    setProfile({ ...profile, trees: [profile.familyTree, newTree],})
+    setProfile({ ...profile, trees: [profile.familyTree, newTree] })
   }
 
-  // useEffect(() => {
-  //   const fetchProfile = async () => {
-  //     const profileData = await profileService.getProfile(profile)
-  //     setProfile(profileData)
-  //   }
-  //   fetchProfile()
-  // }, [profile])
+  useEffect(() => {
+    const fetchProfile = async () => {
+      const profileData = await profileService.getProfile(profileId)
+      setProfile(profileData)
+    }
+    fetchProfile()
+  }, [profileId, profile])
 
   return (
     <>
       <NewTree 
         handleAddTree={handleAddTree}
       />
-      {/* <p>{props.user.profile}</p> */}
+      <p>my tree id: </p>
     </>
   )
 }
