1601d1c6b640393a5f7d737933f7e0b9688abe81
trentonwahr <trentwahr@gmail.com>
2023-05-14 14:14:59 -0400

    adds recipe card and some base styling

--- src/components/RecipeCard/RecipeCard.jsx
+++ src/components/RecipeCard/RecipeCard.jsx
@@ -1,10 +1,13 @@
 // npm modules
 import { Link } from "react-router-dom"
 
+// css
+import styles from './RecipeCard.module.css'
+
 const RecipeCard = ({ recipe }) => {
   return (  
     <Link to={`/recipes/${recipe._id}`}>
-      <article>
+      <article className={styles.container}>
         <header>
           <span>
             <h1>{recipe.name}</h1>
