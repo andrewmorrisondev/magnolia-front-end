f0ec3e207e58e6de060e718da9fbd2a029bad254
nonchalamment <nonchalamment@gmail.com>
2023-05-16 11:10:08 -0400

    edit recipes list page

--- src/pages/RecipesList/RecipesList.jsx
+++ src/pages/RecipesList/RecipesList.jsx
@@ -7,12 +7,18 @@ import styles from './RecipesList.module.css'
 
 const RecipesList = (props) => {
   return (
-    <main className={styles.container}>
-      <NewRecipe handleAddRecipe={props.handleAddRecipe} />
-      {props.recipes.map(recipe => (
-        <RecipeCard key={recipe._id} recipe={recipe} />
-      ))}
-    </main>
+    <>
+      <main className={styles.container}>
+        <div className={styles.formContainer}>
+          <NewRecipe handleAddRecipe={props.handleAddRecipe} />
+        </div>
+        <div className={styles.recipeCardContainer}>
+          {props.recipes.map(recipe => (
+            <RecipeCard key={recipe._id} recipe={recipe} />
+          ))}
+        </div>
+      </main>
+    </>
   )
 }
 
