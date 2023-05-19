5476e5c68ad9c392362f077c198490dceca5cc5c
Andrew Morrison <morrison.andrew422@gmail.com>
2023-05-18 23:37:25 -0400

    fix link rendering

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -7,13 +7,13 @@ import styles from './Landing.module.css'
 // image
 import magnolia from '../../assets/icons/noun-magnolia-5449337.png'
 
-const Landing = ({ user }) => {
+const Landing = ({ user, tree }) => {
   return (
     <main className={styles.container}>
       <h1>Magnolia</h1>
       <img src={magnolia} />
       <h2>{user ? 'Welcome, ' + user.name + '!' : 'Welcome, guest!'}</h2>
-      {user && <Link className={styles.profileLink} to={`/profiles/${user.profile}`}>Create Your Family Tree!</Link>}
+      {(user && tree.length === 0) && <Link className={styles.profileLink} to={`/profiles/${user.profile}`}>Create Your Family Tree!</Link>}
     </main>
   )
 }
